67b2d81c0d790af17f5dce3b32851caacce91ca6
gjmhmm8 <73775469+gjmhmm8@users.noreply.github.com>
2025-09-24 18:23:28 +0800

    净化水转化配方 (#183)

--- src/main/java/com/gtocore/data/recipe/gtm/chemistry/BrewingRecipes.java
+++ src/main/java/com/gtocore/data/recipe/gtm/chemistry/BrewingRecipes.java
@@ -1,7 +1,7 @@
 package com.gtocore.data.recipe.gtm.chemistry;
 
 import com.gregtechceu.gtceu.api.data.chemical.material.Material;
-
+import com.gregtechceu.gtceu.api.machine.multiblock.CleanroomType;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
@@ -10,11 +10,12 @@ import static com.gregtechceu.gtceu.api.data.tag.TagPrefix.dust;
 import static com.gregtechceu.gtceu.common.data.GTItems.BIO_CHAFF;
 import static com.gregtechceu.gtceu.common.data.GTMaterials.*;
 import static com.gtocore.common.data.GTORecipeTypes.BREWING_RECIPES;
+import static com.gtocore.common.machine.multiblock.water.WaterPurificationPlantMachine.GradePurifiedWater;
 
 final class BrewingRecipes {
 
     public static void init() {
-        for (Material material : new Material[] { Talc, Soapstone, Redstone }) {
+        for (Material material : new Material[]{Talc, Soapstone, Redstone}) {
             BREWING_RECIPES.recipeBuilder("lubricant_from_oil_and_" + material.getName())
                     .inputItems(dust, material)
                     .inputFluids(Oil.getFluid(1000))
@@ -74,5 +75,17 @@ final class BrewingRecipes {
         BREWING_RECIPES.recipeBuilder("biomass_from_torchflower_seeds").duration(160).EUt(3)
                 .inputItems(Items.TORCHFLOWER_SEEDS)
                 .inputFluids(Water.getFluid(20)).outputFluids(Biomass.getFluid(20)).save();
+
+        //water
+        for (int i = 1; i < 8; i++)
+            for (int j = 0; j < i; j++) {
+                BREWING_RECIPES.builder("water_%s_%s".formatted(i, j))
+                        .inputFluids(GradePurifiedWater[i], 1000)
+                        .outputFluids(GradePurifiedWater[j], 1000)
+                        .circuitMeta(j+1)
+                        .duration(20)
+                        .cleanroom(CleanroomType.CLEANROOM)
+                        .save();
+            }
     }
 }
