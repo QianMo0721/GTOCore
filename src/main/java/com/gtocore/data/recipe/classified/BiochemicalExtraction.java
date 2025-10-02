31e36732b5bc200bad2169b810bc9bbb212d0899
remakefactory <remakefactory5@gmail.com>
2025-10-02 12:08:19 +0800

    修配方 (#207)

--- src/main/java/com/gtocore/data/recipe/classified/BiochemicalExtraction.java
+++ src/main/java/com/gtocore/data/recipe/classified/BiochemicalExtraction.java
@@ -1,11 +1,9 @@
 package com.gtocore.data.recipe.classified;
 
+import com.gregtechceu.gtceu.common.data.GTMaterials;
 import com.gtocore.common.data.GTOItems;
 import com.gtocore.common.data.GTOMaterials;
 import com.gtocore.data.tag.Tags;
-
-import com.gregtechceu.gtceu.common.data.GTMaterials;
-
 import net.minecraft.world.item.Items;
 
 import static com.gtocore.common.data.GTORecipeTypes.BIOCHEMICAL_EXTRACTION_RECIPES;
@@ -99,7 +97,7 @@ final class BiochemicalExtraction {
                 .save();
 
         BIOCHEMICAL_EXTRACTION_RECIPES.builder("feather")
-                .inputItems(Items.PARROT_SPAWN_EGG.asItem())
+                .notConsumable(Items.PARROT_SPAWN_EGG.asItem())
                 .outputItems(Items.FEATHER.asItem(), 4)
                 .EUt(30)
                 .duration(200)
