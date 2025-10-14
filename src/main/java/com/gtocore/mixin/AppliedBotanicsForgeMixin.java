3ff1f24c13df197ae7e6294b0cf4ce7989bea32a
remakefactory <remakefactory5@gmail.com>
2025-10-14 12:35:59 +0800

    fix Gradle (#229)
    Co-authored-by: gjmhmm8

--- src/main/java/com/gtocore/mixin/AppliedBotanicsForgeMixin.java
+++ src/main/java/com/gtocore/mixin/AppliedBotanicsForgeMixin.java
@@ -22,17 +22,7 @@ public abstract class AppliedBotanicsForgeMixin {
                        target = "Lnet/minecraftforge/fml/DistExecutor;safeRunWhenOn(Lnet/minecraftforge/api/distmarker/Dist;Ljava/util/function/Supplier;)V"))
     private void injectAndReplaceClientInit(Dist dist, Supplier<DistExecutor.SafeRunnable> toRun) {
         if (FMLLoader.getDist() == Dist.CLIENT) {
-            try {
-                // 1. 使用字符串获取 Class 对象，避免直接引用
-                Class<?> clientHandlerClass = Class.forName("com.gtocore.client.ClientSideHandler");
-
-                // 2. 使用字符串获取 Method 对象
-                Method clientInitMethod = clientHandlerClass.getMethod("runClientSideInitialization");
-
-                // 3. 调用静态方法
-                clientInitMethod.invoke(null); // 第一个参数是 null 因为方法是静态的
-
-            } catch (Exception ignored) {}
+            appbot.forge.client.AppliedBotanicsClient.initialize();
         }
     }
 }
